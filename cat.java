1b7e29ba0df69faea888b02571cf821e0e3aab8e
Alexander Zemskov <zaismit@gmail.com>
2019-02-16 18:33:09 +0200

    Add files via upload

---
+++ cat.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ZAISMIT - EVYNET
+ **/
+public class cat extends cats_family
+{
+    public cat()
+    {
+        setbig_claws (false);
+    }
+}
---
+++ lion.java
@@ -0,0 +1,7 @@
+/**
+ * Created by ZAISMIT - EVYNET
+ **/
+public class lion extends cats_family
+{
+
+}
